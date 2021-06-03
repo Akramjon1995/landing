ecf21b456f2308e0f358a7522662c7b165fd8079
Akramjon1995 <akramjonakramov96@gmail.com>
2021-06-03 10:59:40 +0500

    simple

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Header />
       <StickerTable />
       <HardestToComplete />
+      
     </div>
   );
 }
--- src/components/HardestToComplete.js
+++ src/components/HardestToComplete.js
@@ -7,16 +7,16 @@ const HardestToComplete = () => {
     return (
         <div className="hardes-to-complete d-flex justify-content-center align-items-center flex-column p-4 my-5">
             <div>
-                <h1 className="text-center text-white">Hardest to complete</h1>
-                <div className="text-center text-white">
-                    <p>
-                        as one of the biggest song competitions in the world,
+                <h1 className= "text-center text-white">Hardest to complete</h1>
+                <div className="align-items-center d-flex flex-column my-5 paragraph text-center text-white">
+                    <p className="text-start">
+                        As one of the biggest song competitions in the world,
                         Eurovision has been produced iconic songs and artists that we still
-                        listen to this days
+                        listen to this days.
                     </p>
-                    <p>
+                    <p className="text-start">
                         But have you ever wonder which songs and artists are the most
-                        popular in the history of Eurovision
+                        popular in the history of Eurovision?
                     </p>
                 </div>
 
